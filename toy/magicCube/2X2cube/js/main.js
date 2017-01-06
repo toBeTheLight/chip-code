b9ebd4c5687bd80017095fe2492d2d6d658df549
toBeTheLight <yl1991web@126.com>
2017-01-06 23:43:55 +0800

    tools.js修改

--- toy/magicCube/2X2cube/js/main.js
+++ toy/magicCube/2X2cube/js/main.js
@@ -14,16 +14,25 @@ elWrapPosition();
 let elBody = document.body;
 let startX,startY;
 elBody.addEventListener('mousedown',function(ev){
+	elWrap.style.transition="0";
 	let e = ev||event;
 	startX = e.clientX;
 	startY = e.clientY;
 	elBody.addEventListener('mousemove',rotateWrap,false);
 },false);
 function rotateWrap(ev){
-	let e = ev||event;
-	let nowX = e.clientX;
-	let nowY = e.clientY;
-	let moveX = nowX - startX;
-	let moveY = nowY - startY;
+	let e = ev||event,
+	nowX = e.clientX,
+	nowY = e.clientY,
+	moveX = nowX - startX,
+	moveY = nowY - startY;
+	startX = nowX;
+	startY = nowY;
 	//做角度转换
+	let rotateY = moveX*2,
+	rotateX = -moveY*2;
+	console.log(elWrap.transformTool('rotate'))
+	elWrap.transformTool('rotateY',rotateY+elWrap.transformTool('rotateY'));
+	elWrap.transformTool('rotateX',rotateX+elWrap.transformTool('rotateX'));
+	
 }
--- toy/magicCube/2X2cube/js/tools.js
+++ toy/magicCube/2X2cube/js/tools.js
@@ -1,13 +1,14 @@
 'use strict'
-Element.prototype.transform = function(type,number){
+Element.prototype.transformTool = function(type, number) {
+	if(this.transformObj === undefined) this.transformObj = {};
 	let unit = '';
-	if(/^(rotate|skew)/.test(type))unit = 'deg';
-	if(/^translate/.test(type))unit = 'px';
-	this.type = number;
-	if(number===undefined){
-		return this.type
-	}else{
-		this.style.transform = type+'('+number+unit+')';
+	if(/^(rotate|skew)/.test(type)) unit = 'deg';
+	if(/^translate/.test(type)) unit = 'px';
+	this.transformObj. = {};
+	console.log(this)
+	if(number === undefined) {
+		return this.transformObj.type||0
+	} else {
+		this.style.transform = type + '(' + number + unit + ')';
 	}
-	console.log(this.type)
 }

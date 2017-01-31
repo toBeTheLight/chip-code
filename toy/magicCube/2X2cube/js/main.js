edaadf3cb265102e7cf79d2e2efad60d2ff1cb5b
toBeTheLight <yl1991web@126.com>
2017-02-01 00:39:57 +0800

    完成整体键盘控制旋转

--- toy/magicCube/2X2cube/js/main.js
+++ toy/magicCube/2X2cube/js/main.js
@@ -1,65 +1,121 @@
 'use strict'
-//let elWrap = document.getElementsByClassName('wrap')[0];
-let elWrap = $my('wrap')
-//wrap位置修正
-function elWrapPosition() {
-	elWrap.css('transitionDuration','0s');
-	console.log(elWrap.doms[0].style.transitionDuration)
-	let elWrapLeft = document.body.clientWidth / 2 - 100,
-	elWrapTop = document.body.clientHeight / 2 - 100,
-	elWrapPositionText = 'left:' + elWrapLeft + 'px;top:' + elWrapTop + 'px';
-//	elWrap.setAttribute('style', elWrapPositionText);
-	elWrap.css('left',elWrapLeft + 'px');
-	elWrap.css('top', elWrapTop + 'px');
-}
-window.addEventListener('resize', elWrapPosition, false)
-elWrapPosition();
-//初始化颜色对象
-let colorOfX = {
-	face:'deepskyblue',
-	right:'green',
-	back:'silver',
-	left:'yellow'
-}
-let colorOfY = {
-	face:'deepskyblue',
-	top:'orange',
-	back:'silver',
-	bottom:'deeppink'
-}
-//键盘事件，固定角度旋转
-let rotateWrap = (ev)=>{
-	//恢复transition效果
-	elWrap.css('transition','all 0.3s');
-	let e = ev || event;
-	let x = 0,y = 0;
-	switch (e.keyCode){
-		case 87 :
-		//按下w 上
-		console.log('上');
-		x = 90;
-		break;
-		case 83 :
-		//按下s 下
-		console.log('下');
-		x = -90;
-		break;
-		case 65 :
-		//按下a 左
-		console.log('左');
-		y = -90;
-		break;
-		case 68 :
-		//按下d 右
-		console.log('右');
-		y = 90;
-		break;
+
+let elWrap = $my('wrap');
+//wrap位置修正部分
+(function() {
+	function elWrapPosition() {
+		elWrap.css('transitionDuration', '0s');
+		console.log(elWrap.doms[0].style.transitionDuration)
+		let elWrapLeft = document.body.clientWidth / 2 - 100,
+			elWrapTop = document.body.clientHeight / 2 - 100,
+			elWrapPositionText = `left:${elWrapLeft}px;top:${elWrapTop}px`;
+		elWrap.css('left', `${elWrapLeft}px`);
+		elWrap.css('top', `${elWrapTop}px`);
 	}
-	elWrap.transformTool('rotateX', x + elWrap.transformTool('rotateX'));
-	elWrap.transformTool('rotateY', y + elWrap.transformTool('rotateY'));
-	
-}
+	window.addEventListener('resize', elWrapPosition, false)
+	elWrapPosition();
+})();
 
-window.addEventListener('keyup',rotateWrap,false);
+//wasd控制整体旋转部分
+(function() {
+	//初始化颜色对象
+	let colorOf = {
+			face: 'deepskyblue',
+			right: 'green',
+			back: 'silver',
+			left: 'yellow',
+			top: 'orange',
+			bottom: 'deeppink'
+		},
+		//键盘事件，固定角度旋转
+		rotateWrap = (ev) => {
+			window.removeEventListener('keyup',rotateWrap,false);
+			//恢复transition效果
+			elWrap.css('transition', 'all 0.3s');
+			let e = ev || event;
+			let x = 0,
+				y = 0;
+			switch(e.keyCode) {
+				case 87:
+					//按下w 上
+					x = 90;
+					break;
+				case 83:
+					//按下s 下
+					x = -90;
+					break;
+				case 65:
+					//按下a 左
+					y = -90;
+					break;
+				case 68:
+					//按下d 右
+					y = 90;
+					break;
+			}
 
-
+			let rotateStr = `rotateX(${x}deg) rotateY(${y}deg)`;
+			elWrap.css('transform', rotateStr);
+			//颜色更改,待优化
+			let colorOfTemp = null;
+			console.log(x, y);
+			if(x) {
+				if(x < 0) {
+					colorOfTemp = {
+						face: colorOf.top,
+						top: colorOf.back,
+						back: colorOf.bottom,
+						bottom: colorOf.face,
+						right: colorOf.right,
+						left: colorOf.left
+					}
+				} else {
+					colorOfTemp = {
+						face: colorOf.bottom,
+						top: colorOf.face,
+						back: colorOf.top,
+						bottom: colorOf.back,
+						right: colorOf.right,
+						left: colorOf.left
+					}
+				}
+			} else if(y) {
+				if(y < 0) {
+					colorOfTemp = {
+						face: colorOf.right,
+						right: colorOf.back,
+						back: colorOf.left,
+						left: colorOf.face,
+						top: colorOf.top,
+						bottom: colorOf.bottom
+					}
+				} else {
+					colorOfTemp = {
+						face: colorOf.left,
+						right: colorOf.face,
+						back: colorOf.right,
+						left: colorOf.back,
+						top: colorOf.top,
+						bottom: colorOf.bottom
+					}
+				};
+			};
+			colorOf = colorOfTemp;
+			colorOfTemp = null;
+			x = 0;
+			y = 0;
+			let transitionCallback = () => {
+				$my('face').css('background-color', colorOf.face);
+				$my('top').css('background-color', colorOf.top);
+				$my('back').css('background-color', colorOf.back);
+				$my('bottom').css('background-color', colorOf.bottom);
+				$my('left').css('background-color', colorOf.left);
+				$my('right').css('background-color', colorOf.right);
+				elWrap.css('transition', 'all 0s');
+				elWrap.css('transform', '');
+				window.addEventListener('keyup', rotateWrap, false);
+			}
+			elWrap.on('transitionend', transitionCallback, false);
+		};
+	window.addEventListener('keyup', rotateWrap, false);
+})();
--- toy/magicCube/2X2cube/js/tools.js
+++ toy/magicCube/2X2cube/js/tools.js
@@ -1,47 +1,6 @@
-//
-//transformTool使用说明：
-//未传入参数则返回保存对象
-//传入一个参数为获取对应值
-//传入两个参数为设置对应值
-//Element.prototype.transformTool = function(type, number) {
-//	//初次调用，对对象添加数据保存对象
-//	if(thisDoms[i].transformObj === undefined) {
-//		thisDoms[i].transformObj = {}
-//	};
-//	if(type === undefined) {
-//		return thisDoms[i].transformObj;
-//		//未传入number参数，则按照查询处理
-//	} else if(number === undefined) {
-//		//对应变换类型不存在，直接返回0
-//		if(thisDoms[i].transformObj[type] === undefined) {
-//			return 0
-//				//对应变换类型存在，返回保存值
-//		} else {
-//			return thisDoms[i].transformObj[type].value;
-//		}
-//	} else {
-//		//进行单位判断
-//		let unit = '';
-//		if(/^(rotate|skew)/.test(type)) unit = 'deg';
-//		if(/^translate/.test(type)) unit = 'px';
-//		//将欲设置值保存至数据保存对象
-//		thisDoms[i].transformObj[type] = {
-//			value: number,
-//			unit: unit
-//		};
-//		//遍历数据保存对象拼接transform字符串
-//		let transformStr = '';
-//		for(let tempType in thisDoms[i].transformObj) {
-//			let tempObj = thisDoms[i].transformObj[tempType];
-//			transformStr += ' ' + tempType + '(' + tempObj.value + tempObj.unit + ')';
-//		}
-//		//进行完整样式设置
-//		this.style.transform = transformStr;
-//	}
-//};
-
 (function(window) {
 	'use strict'
+	//初步先完成类名选择器
 	window.$my = (className) => {
 		return(new MyTools(className));
 	}
@@ -55,7 +14,17 @@
 			}
 		}
 
+		on(event,fn,bub){
+			for(let i = 0;i<this.doms.length;i++){
+				this.doms[i].addEventListener(event,fn,bub);
+			};
+		}
 		transformTool(type, number) {
+			//transformTool使用说明：
+			//用于transform连续变换，单次变化可使用css
+			//未传入参数则返回保存对象
+			//传入一个参数为获取对应值
+			//传入两个参数为设置对应值
 			for(let i = 0; i < this.doms.length; i++) {
 				//初次调用，对对象添加数据保存对象
 				let thisDoms = this.doms; 
@@ -93,7 +62,6 @@
 					thisDoms[i].style.transform = transformStr;
 				}
 			}
-
 		}
 	}
 })(window)

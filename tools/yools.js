c698440005203331d8a86a4bacdb03cdc955e7e5
toBeTheLight <yl1991web@126.com>
2017-06-11 10:32:51 +0800

    扩展yools

--- tools/yools.js
+++ tools/yools.js
@@ -65,4 +65,55 @@ var yools = {};
 		}
 	}
 	yools.scrollbar = scrollbar;
+	
+	//列表类active切换
+	function changeActiveItemOfList($dom,className){
+		$($dom).addClass(className).siblings().removeClass(className);
+	};
+	yools.changeActiveItemOfList = changeActiveItemOfList;
+	
+	//判断是否在视口内
+		//y:高度差(还有y高度时同样认为离开视口)
+	function isOnScreen(selector,y){
+		if(y==undefined){
+			y = 0;
+		};
+	    var win = $(window);
+	    var viewport = {
+	        top : win.scrollTop(),
+	        left : win.scrollLeft()
+	    };
+	    viewport.right = viewport.left + win.width();
+	    viewport.bottom = viewport.top + win.height();
+		$dom = $(selector);
+		var bounds = $dom.offset();
+	    bounds.right = bounds.left + $dom.outerWidth();
+	    bounds.bottom = bounds.top + $dom.outerHeight();
+	    return (!(viewport.bottom < bounds.top || viewport.top > bounds.bottom-y));
+	     
+	};
+	yools.isOnScreen = isOnScreen;
+	
+	//导航active与内容滚动关联
+		//blockSelector:内容块;navItemSelector:导航元素;className:切换样式;y:高度差
+	yool.toggleNavClassOnScroll = function(blockSelector,navItemSelector,className,y){
+		var _init = function(){
+			var index;
+			for(var i = 0;i<$(blockSelector).length;i++){
+				if(yool.isOnScreen($(blockSelector)[i],y)){
+					index = i;
+					break;
+				}
+			};
+			if(i>=$(blockSelector).length){
+				$(navItemSelector).removeClass(className);
+				return false;
+			};
+			yool.changeActiveItemOfList($(navItemSelector)[index],className);
+		};
+		_init();
+		$(window).scroll(function(){
+			_init();
+		})
+	};
 })();
